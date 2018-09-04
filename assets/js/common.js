efd1ae02dd54320fb91129839c35e82b55ba5261
cmlanche <cmlanche@gmail.com>
2018-09-04 22:42:30 +0800

    add google analytics

--- assets/js/common.js
+++ assets/js/common.js
@@ -12,4 +12,23 @@ document.addEventListener('DOMContentLoaded', function (e) {
             }
         }
     }
+
+    initGoogleAnasystic();
 });
+
+// 初始化谷歌统计
+function initGoogleAnasystic() {
+    var id = config.google_analytics_id;
+    // 因为要传递一个动态的统计ID，所以要懒加载gtag.js库
+    LazyLoad.js('https://www.googletagmanager.com/gtag/js?id=' + id, function(){
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', id);
+
+        window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+        ga('create', id, 'auto');
+        // ga('require', 'displayfeatures');
+        ga('send', 'pageview');
+    })
+}
